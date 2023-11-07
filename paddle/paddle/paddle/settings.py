7a60f410601697726a02f1aa1fcc14c9b2787ee3
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-11-07 18:14:43 +0200

    comments here

--- paddle/paddle/paddle/settings.py
+++ paddle/paddle/paddle/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-database_url = os.environ.get("DATABASE_URL")
-DATABASES ['default'] = dj_database_url.parse(database_url) 
+##database_url = os.environ.get("DATABASE_URL")
+#DATABASES ['default'] = dj_database_url.parse(database_url) 
 
  
 
