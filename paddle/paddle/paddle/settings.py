c5c3caba02dc0167debfdbcfa9028fe644be38d2
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-11-07 17:19:46 +0200

    comments here

--- paddle/paddle/paddle/settings.py
+++ paddle/paddle/paddle/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import  dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-thq13m1=ws%=5w$t9@roa4w16__p)33&d8wsgj3-*nx^ejf5xc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -82,15 +84,19 @@ WSGI_APPLICATION = 'paddle.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'paddle', 
-        'USER': 'root',  #postgres
-        'PASSWORD': '070894', 
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'paddle', 
+       'USER': 'root',  #postgres
+       'PASSWORD': '070894', 
         'HOST': 'localhost', #localhost
         'PORT': '3306',
     }
 }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES ['default'] = dj_database_url.parse(database_url) 
+
+ 
 
 
 # Password validation
--- paddle/paddle/paddle/urls.py
+++ paddle/paddle/paddle/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import index, contact
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -27,4 +28,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('conversation/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
+                  
+urlpatterns+=staticfiles_urlpatterns()
